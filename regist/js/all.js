6f08173a34d8d2b8267f00a13bc9377f1ae46622
윤주형 <sub0709@gmail.com>
2020-04-24 16:37:44 +0900

    예제 추가

---
+++ regist/js/all.js
@@ -0,0 +1,11 @@
+$(function() {
+	$('.button').click(function() {
+		$('.wrong-id').addClass('act');
+	});
+	$('.wrong').click(function() {
+		$('.wrong-in').addClass('act');
+	});
+	$('.pop').click(function() {
+		$('#pop').show();
+	});
+}); 
