d5c6ac1c847cb6d1891e86b4b9c76222e073a541
윤주형 <sub0709@gmail.com>
2020-04-27 10:04:42 +0900

    q5

---
+++ classix/assets/js/data.js
@@ -0,0 +1,51 @@
+function getMenuInfos(callback) {
+	callback([
+		{ "href" : "index.html", "menu" : "Home" },{ "href" : "index-v-2.html", "menu" : "Home Page V2" },{ "href" : "about.html", "menu" : "About us" },{ "href" : "category.html", "menu" : "Category" },{ "href" : "ads-details.html", "menu" : "Ads details" },{ "href" : "pricing.html", "menu" : "Pricing Tables" },{ "href" : "account-archived-ads.html", "menu" : "Account archived" },{ "href" : "account-close.html", "menu" : "Account-close" },{ "href" : "account-favourite-ads.html", "menu" : "Favourite ads" },{ "href" : "account-home.html", "menu" : "Account home" },{ "href" : "account-myads.html", "menu" : "Account myads" },{ "href" : "account-pending-approval-ads.html", "menu" : "pending approval" },{ "href" : "account-saved-search.html", "menu" : "saved search" },{ "href" : "post-ads.html", "menu" : "Post ads" },{ "href" : "posting-success.html", "menu" : "Posting-success" },{ "href" : "blog.html", "menu" : "Blogs" },{ "href" : "blog-details.html", "menu" : "Blog Details" },{ "href" : "contact.html", "menu" : "Contact" },{ "href" : "forgot-password.html", "menu" : "Forgot-password" },{ "href" : "faq.html", "menu" : "Faq" },{ "href" : "signup.html", "menu" : "Signup" }
+	]);
+}
+
+
+function getFeaturedList(callback) {
+	callback([
+		{
+			img : 'assets/img/product/img1.jpg',
+			'item-name' : 'Lorem ipsum dolor sit',
+			price : 150
+		},
+		{
+			img : 'assets/img/product/img2.jpg',
+			'item-name' : 'Sed diam nonummy',
+			price : 67
+		},
+		{
+			img : 'assets/img/product/img3.jpg',
+			'item-name' : 'Feugiat nulla facilisis',
+			price : 300
+		},
+		{
+			img : 'assets/img/product/img4.jpg',
+			'item-name' : 'Lorem ipsum dolor sit',
+			price : 149
+		},
+		{
+			img : 'assets/img/product/img5.jpg',
+			'item-name' : 'Sed diam nonummy',
+			price : 90
+		},
+		{
+			img : 'assets/img/product/img6.jpg',
+			'item-name' : 'Praesent luptatum zzril',
+			price : 169
+		},
+		{
+			img : 'assets/img/product/img7.jpg',
+			'item-name' : 'Lorem ipsum dolor sit',
+			price : 79
+		},
+		{
+			img : 'assets/img/product/img8.jpg',
+			'item-name' : 'Sed diam nonummy',
+			price : 149
+		},
+	])
+}
